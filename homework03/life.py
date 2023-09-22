3ed6bcfc97f76538770bce586e43192b5d1ea5ab
PfLoLexe <volodin3002@gmail.com>
2023-09-22 20:15:29 +0300

    Elistratov_Vladimir_K33392_LR3

--- homework03/life.py
+++ homework03/life.py
@@ -17,6 +17,7 @@ class GameOfLife:
         randomize: bool = True,
         max_generations: tp.Optional[float] = float("inf"),
     ) -> None:
+
         # Размер клеточного поля
         self.rows, self.cols = size
         # Предыдущее поколение клеток
@@ -29,46 +30,95 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        grid = [0] * self.rows
+        
+        for i in range(self.rows):
+            grid[i] = [0] * self.cols
+            if(randomize):
+                for j in range(self.cols):
+                    grid[i][j] = random.choice((0, 1))
+
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        addcell = []
+        row = cell[0]
+        col = cell[1]
+        for i in range(row - 1, row + 2):
+            for j in range(col - 1, col + 2):
+                if not (0 <= i <= self.rows - 1 and 0 <= j <= self.cols - 1) or (
+                    i == row and j == col
+                ):
+                    continue
+                addcell.append(self.curr_generation[i][j])
+        return addcell
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        subgrid = self.create_grid(False)
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                cell = (i, j)
+                if (self.curr_generation[i][j] == 0) and sum(self.get_neighbours(cell)) == 3:
+                    subgrid[i][j] = 1
+                elif (self.curr_generation[i][j] == 1) and (
+                    2 <= sum(self.get_neighbours(cell)) <= 3
+                ):
+                    subgrid[i][j] = 1
+        self.curr_generation = subgrid
+        return subgrid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        self.prev_generation = self.curr_generation
+        self.curr_generation = self.get_next_generation()
+        self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        if self.max_generations <= self.generations:  # type: ignore
+            return True
+        else:
+            return False
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        if self.prev_generation != self.curr_generation:
+            return True
+        else:
+            return False
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        fileforopen = open(filename, "r")
+        filetext = [[int(col) for col in row.strip()] for row in fileforopen]
+        fileforopen.close()
+        gamefromfile = GameOfLife((len(filetext), len(filetext[0])))
+        gamefromfile.curr_generation = filetext
+        return gamefromfile
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+
+        fileforsave = open(filename, "w")
+
+        for rows in self.curr_generation:
+            for cols in rows:
+                fileforsave.write(str(cols))
+
+            fileforsave.write("\n")
+
+        fileforsave.close()
--- homework03/life_console.py
+++ homework03/life_console.py
@@ -10,13 +10,35 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border("|", "|", "-", "-", "+", "+", "+", "+")
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for rows in range(self.life.cols):
+            for cols in range(self.life.rows):
+                try:
+
+                    if self.life.curr_generation[rows][cols]:
+                        screen.addch(rows + 1, cols + 1, "*")
+
+                except:
+                    pass
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        pause = False
+
+        while self.life.is_changing and not self.life.is_max_generations_exceeded:
+            screen.clear()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            self.life.step()
+            screen.refresh()
+
         curses.endwin()
+
+
+if __name__ == "__main__":
+    game = GameOfLife((24, 80), max_generations=50)
+    ui = Console(game)
+    ui.run()

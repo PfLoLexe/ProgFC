2f6868b5011d1ca5d7a59eac0ac4566133799b5c
PfLoLexe <volodin3002@gmail.com>
2023-09-22 20:17:23 +0300

    Elistratov_Vladimir_K33392_LR4

--- homework04/pyvcs/index.py
+++ homework04/pyvcs/index.py
@@ -26,29 +26,78 @@ class GitIndexEntry(tp.NamedTuple):
 
     def pack(self) -> bytes:
         # PUT YOUR CODE HERE
-        ...
+        return struct.pack((f"!10I20sH{ len(self.name) }s{ 8 - ( 62 + len(self.name) ) % 8 }x"), self.ctime_s, self.ctime_n, self.mtime_s, self.mtime_n, self.dev, self.ino & 0xFFFFFFFF, self.mode, self.uid, self.gid, self.size, self.sha1, self.flags, self.name.encode(),)
 
     @staticmethod
     def unpack(data: bytes) -> "GitIndexEntry":
         # PUT YOUR CODE HERE
-        ...
+        date = list(struct.unpack(f"!10I20sH{len(data) - 62}s", data))
+        date[len(date) - 1] = date[len(date) - 1].strip(b"\00").decode()
+
+        return GitIndexEntry(*date)
 
 
 def read_index(gitdir: pathlib.Path) -> tp.List[GitIndexEntry]:
     # PUT YOUR CODE HERE
-    ...
+    if not ((gitdir / "index").exists()):
+        return []
+
+    with (gitdir / "index").open("rb") as fileforrep:
+        read = fileforrep.read()
+
+    flastpos = 12
+    res = []
+
+    for it in range(struct.unpack("!I", read[8:12])[0]):
+        slastpos = read.index(b"\00", flastpos + 62)
+
+        while ((slastpos - 11) % 8 != 0) or (read[slastpos] != 0):
+            slastpos += 1
+
+        res.append(GitIndexEntry.unpack(read[flastpos : slastpos + 1]))
+
+        flastpos = slastpos + 1
+
+    return res
 
 
 def write_index(gitdir: pathlib.Path, entries: tp.List[GitIndexEntry]) -> None:
     # PUT YOUR CODE HERE
-    ...
+    inf = b"DIRC" + struct.pack("!2I", 2, len(entries))
+
+    for i in entries:
+        inf += i.pack()
+
+    inf += hashlib.sha1(inf).digest()
+    with (gitdir / "index").open("wb") as fileforrep:
+        fileforrep.write(inf)
 
 
 def ls_files(gitdir: pathlib.Path, details: bool = False) -> None:
     # PUT YOUR CODE HERE
-    ...
+    readinf = read_index(gitdir)
+
+    if not details:
+        for j in readinf:
+            print(j.name)
+
+    else:
+        for i in readinf:
+            print(f"{i.mode:o} {i.sha1.hex()} 0\t{i.name}")
 
 
 def update_index(gitdir: pathlib.Path, paths: tp.List[pathlib.Path], write: bool = True) -> None:
     # PUT YOUR CODE HERE
-    ...
+    readinf = read_index(gitdir)
+
+    for i in paths:
+        with i.open("rb") as fileforrep:
+            inf = fileforrep.read()
+
+        hash = hash_object(inf, "blob", write=True)
+        stat = i.stat()
+
+        readinf.append(GitIndexEntry(ctime_s=int(stat.st_ctime), ctime_n=0, mtime_s=int(stat.st_mtime), mtime_n=0, dev=stat.st_dev, ino=stat.st_ino, mode=stat.st_mode, uid=stat.st_uid, gid=stat.st_gid, size=stat.st_size, sha1=bytes.fromhex(hash), flags=len(i.name), name=str(i),))
+
+    if write:
+        write_index(gitdir, sorted(readinf, key=lambda j: j.name))
--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -3,21 +3,68 @@ import pathlib
 import typing as tp
 
 from pyvcs.index import read_index, update_index
-from pyvcs.objects import commit_parse, find_object, find_tree_files, read_object
+from pyvcs.objects import commit_parse, find_object, find_tree_files, read_object, read_tree
 from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref
 from pyvcs.tree import commit_tree, write_tree
 
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
     # PUT YOUR CODE HERE
-    ...
+    for i in paths:
+        if not (i.is_dir()):
+            update_index(gitdir, [i], write=True)
+
+        else:
+            add(gitdir, list(i.glob("*")))
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
     # PUT YOUR CODE HERE
-    ...
+    parentcom = resolve_head(gitdir)
+    comtree = write_tree(gitdir, read_index(gitdir), str(gitdir.parent))
+    hashcom = commit_tree(gitdir, comtree, message, parentcom, author)
+    return hashcom
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
     # PUT YOUR CODE HERE
-    ...
+    for i in read_index(gitdir):
+        if pathlib.Path(i.name).exists():
+            os.remove(i.name)
+
+    closeprog = False
+    datacom = commit_parse(read_object(obj_name, gitdir)[1])
+
+    while not closeprog:
+        tree: tp.List[tp.Tuple[pathlib.Path, tp.List[tp.Tuple[int, str, str]]]] = [
+            (gitdir.parent, read_tree(read_object(datacom["tree"], gitdir)[1]))
+        ]
+
+        while tree:
+            pathoftree, treeinf = tree.pop()
+
+            for j in treeinf:
+                form, data = read_object(j[1], gitdir)
+                if not (form == "tree"):
+                    if not (pathoftree / j[2]).exists():
+                        with (pathoftree / j[2]).open("wb") as fileforrep:
+                            fileforrep.write(data)
+                        (pathoftree / j[2]).chmod(int(str(j[0]), 8))
+
+                else:
+                    tree.append((pathoftree / j[2], read_tree(data)))
+                    if not (pathoftree / j[2]).exists():
+                        (pathoftree / j[2]).mkdir()
+
+        if not ("parent" in datacom):
+            closeprog = True
+
+        else:
+            datacom = commit_parse((read_object(datacom["parent"], gitdir)[1]))
+
+    for dir in gitdir.parent.glob("*"):
+        if dir != gitdir and dir.is_dir():
+            try:
+                os.removedirs(dir)
+            except OSError:
+                continue
